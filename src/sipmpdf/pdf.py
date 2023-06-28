2030c7017bdf300cc30f7d36ebcc32e3570c65ed
grace-blip <grace.finlayson@rutgers.edu>
2023-06-28 23:21:26 +0200

    Creation of pdf.py

---
+++ src/sipmpdf/pdf.py
@@ -0,0 +1,21 @@
+"""
+
+pdf.py
+
+TODO: insert description of functions here
+
+"""
+
+import zfit
+
+class DemoSiPMPDF(zfit.pdf.ZPDF):
+  _N_OBS = 1
+  _PARAMS = [
+    'ap_beta', 'ap_prob', 'pedestal', 'gain','poisson_mean','poisson_borel','common_noise','pixel_noise','dc_prob','dc_res'
+  ]
+
+  def _unnormalized_pdf(self, x):
+    x = zfit.z.unstack_x(x)
+    return f.sipm_response(
+      x, **{k: self.params[k]
+            for k in self._PARAMS})
