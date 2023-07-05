ff46b35f88adf53b809908d1e5ec11e04a27f191
grace-blip <grace.finlayson@rutgers.edu>
2023-07-05 22:45:45 +0200

    Edit to pdf and fixed typo in functions.py

--- src/sipmpdf/pdf.py
+++ src/sipmpdf/pdf.py
@@ -1,11 +1,12 @@
 """
 pdf.py
 
-TODO: insert description of functions here
+
 
 """
 
 import zfit
+from . import functions as f
 
 class DemoSiPMPDF(zfit.pdf.ZPDF):
   _N_OBS = 1
@@ -19,3 +20,34 @@ class DemoSiPMPDF(zfit.pdf.ZPDF):
       x, **{k: self.params[k]
             for k in self._PARAMS})
 
+
+class SiPMResponse_NoDCAP_PDF(zfit.pdf.ZPDF):
+  _N_OBS = 1
+  _PARAMS = ['pedestal','gain','common_noise','pixel_noise','poisson_mean','poisson_borel']
+    
+  def _unnormalized_pdf(self, x):
+    x = zfit.z.unstack_x(x)
+    return f.sipm_response_no_dark_no_ap(
+      x, **{k: self.params[k]
+            for k in self._PARAMS})
+
+class SiPMResponse_NoDC_PDF(zfit.pdf.ZPDF):
+  _N_OBS = 1
+  _PARAMS = ['pedestal','gain','common_noise','pixel_noise','poisson_mean','poisson_borel','ap_prob','ap_beta']
+    
+  def _unnormalized_pdf(self, x):
+    x = zfit.z.unstack_x(x)
+    return f.sipm_response_no_dark(
+      x, **{k: self.params[k]
+            for k in self._PARAMS})
+
+
+class SiPMResponsePDF(zfit.pdf.ZPDF):
+  _N_OBS = 1
+  _PARAMS = ['pedestal','gain','common_noise','pixel_noise','poisson_mean','poisson_borel','ap_prob','ap_beta','dc_res','dc_prob']
+    
+  def _unnormalized_pdf(self, x):
+    x = zfit.z.unstack_x(x)
+    return f.sipm_response(
+      x, **{k: self.params[k]
+            for k in self._PARAMS})
