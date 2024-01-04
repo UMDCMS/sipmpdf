5b9fe73ed002a14be794363f147feedec06b40e8
Yi-Mu Chen <enochnotsocool@gmail.com>
2024-01-04 04:15:20 +0100

    Switching to black.isort for formatting

--- src/sipmpdf/pdf.py
+++ src/sipmpdf/pdf.py
@@ -6,44 +6,61 @@ SiPM response functions as a zfit.pdf.ZPDF extended class for user level fits.
 """
 
 import zfit
+
 from . import functions as f
 
 
 class SiPMResponse_NoDCAP_PDF(zfit.pdf.ZPDF):
-  _N_OBS = 1
-  _PARAMS = [
-    'pedestal', 'gain', 'common_noise', 'pixel_noise', 'poisson_mean',
-    'poisson_borel'
-  ]
+    _N_OBS = 1
+    _PARAMS = [
+        "pedestal",
+        "gain",
+        "common_noise",
+        "pixel_noise",
+        "poisson_mean",
+        "poisson_borel",
+    ]
 
-  def _unnormalized_pdf(self, x):
-    x = zfit.z.unstack_x(x)
-    return f.sipm_response_no_dark_no_ap(
-      x, **{k: self.params[k]
-            for k in self._PARAMS})
+    def _unnormalized_pdf(self, x):
+        x = zfit.z.unstack_x(x)
+        return f.sipm_response_no_dark_no_ap(
+            x, **{k: self.params[k] for k in self._PARAMS}
+        )
 
 
 class SiPMResponse_NoDC_PDF(zfit.pdf.ZPDF):
-  _N_OBS = 1
-  _PARAMS = [
-    'pedestal', 'gain', 'common_noise', 'pixel_noise', 'poisson_mean',
-    'poisson_borel', 'ap_prob', 'ap_beta'
-  ]
+    _N_OBS = 1
+    _PARAMS = [
+        "pedestal",
+        "gain",
+        "common_noise",
+        "pixel_noise",
+        "poisson_mean",
+        "poisson_borel",
+        "ap_prob",
+        "ap_beta",
+    ]
 
-  def _unnormalized_pdf(self, x):
-    x = zfit.z.unstack_x(x)
-    return f.sipm_response_no_dark(x,
-                                   **{k: self.params[k]
-                                      for k in self._PARAMS})
+    def _unnormalized_pdf(self, x):
+        x = zfit.z.unstack_x(x)
+        return f.sipm_response_no_dark(x, **{k: self.params[k] for k in self._PARAMS})
 
 
 class SiPMResponsePDF(zfit.pdf.ZPDF):
-  _N_OBS = 1
-  _PARAMS = [
-    'pedestal', 'gain', 'common_noise', 'pixel_noise', 'poisson_mean',
-    'poisson_borel', 'ap_prob', 'ap_beta', 'dc_prob', 'dc_res'
-  ]
+    _N_OBS = 1
+    _PARAMS = [
+        "pedestal",
+        "gain",
+        "common_noise",
+        "pixel_noise",
+        "poisson_mean",
+        "poisson_borel",
+        "ap_prob",
+        "ap_beta",
+        "dc_prob",
+        "dc_res",
+    ]
 
-  def _unnormalized_pdf(self, x):
-    x = zfit.z.unstack_x(x)
-    return f.sipm_response(x, **{k: self.params[k] for k in self._PARAMS})
+    def _unnormalized_pdf(self, x):
+        x = zfit.z.unstack_x(x)
+        return f.sipm_response(x, **{k: self.params[k] for k in self._PARAMS})
